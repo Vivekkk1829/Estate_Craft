7c783667c81ef0fbd80939bc9ed4c6715ce713b8
Vivekkk1829 <vivekkatta1829@gmail.com>
2025-12-14 23:55:31 +0530

    Final

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
+app.options('*', cors());
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
